3f93908a67361bc481389da8416a2cf4d405f6d7
Abdalla Hisham <hazim@snsoerp.com>
2022-02-10 10:14:49 +0200

    add animation to header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React, { useContext } from "react";
 import { BookContext } from "../contexts/BookContext";
+import {motion} from 'framer-motion';
 
 const Navbar = () => {
   const { books } = useContext(BookContext);
   return (
     <div className="navbar">
-      <h1>List of Books</h1>
-      <p>Currently you have {books.length} books.</p>
+      <motion.h1
+      initial={{ y: -200}}
+      animate={{ y:0 }}
+      transition={{ delay:0.1 }}
+      >List of Books</motion.h1>
+      <motion.p
+      initial={{ x: -2000}}
+      animate={{ x:0 }}
+      transition={{ delay:0.2 }}
+      >Currently you have {books.length} books.</motion.p>
     </div>
   );
 };
